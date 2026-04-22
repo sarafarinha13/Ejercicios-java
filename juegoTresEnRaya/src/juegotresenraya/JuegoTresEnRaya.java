3c1c95ac7214459b0f94688dc0677da8bbb338fe
Sara <150589811+sarafarinha13@users.noreply.github.com>
2026-04-22 19:05:27 +0200

    Añadir entrada de usuario y mapeo de posiciones
    El usuario introduce un número que se convierte en coordenadas de la matriz para colocar una 'X' en el tablero

--- juegoTresEnRaya/src/juegotresenraya/JuegoTresEnRaya.java
+++ juegoTresEnRaya/src/juegotresenraya/JuegoTresEnRaya.java
@@ -2,6 +2,7 @@ package juegotresenraya;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class JuegoTresEnRaya {
     // Vamos a usar la clase JuegoTresEnRaya y además el main
@@ -22,6 +23,20 @@ public class JuegoTresEnRaya {
     
     private static Map<Integer, Integer[]> posiciones = new HashMap<>();
     
+    //metodo para cargar posiciones
+    private static void cargarPosiciones(){
+        JuegoTresEnRaya.posiciones.put(1,new Integer[]{0,0});
+        JuegoTresEnRaya.posiciones.put(2,new Integer[]{0,1});
+        JuegoTresEnRaya.posiciones.put(3,new Integer[]{0,2});
+        
+        JuegoTresEnRaya.posiciones.put(4,new Integer[]{1,0});
+        JuegoTresEnRaya.posiciones.put(5,new Integer[]{1,1});
+        JuegoTresEnRaya.posiciones.put(6,new Integer[]{1,2});
+        
+        JuegoTresEnRaya.posiciones.put(7,new Integer[]{2,0});
+        JuegoTresEnRaya.posiciones.put(8,new Integer[]{2,1});
+        JuegoTresEnRaya.posiciones.put(9,new Integer[]{2,2});
+    }
     
     
     
@@ -46,17 +61,36 @@ public class JuegoTresEnRaya {
     
 
 public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     
-    //traemos el nombre de la clase JuegoTresEnRaya
     System.out.println(JuegoTresEnRaya.nombreJuego);
     
-    
     //traemos el método pintar tablero 
+    cargarPosiciones();
     pintarTablero();
-
     
     
-
+    //Aquí le pedimos al usuario que nos ponga la posición de x
+    System.out.println("Elige la posición?");
+    int posicionElegida = sc.nextInt(); 
+    
+    
+    //validamos que el numero que entre sea válido
+    while(posicionElegida<1 || posicionElegida>9){
+        System.out.println("Introduce un número del 1 al 9");
+        posicionElegida = sc.nextInt(); 
+    }
+    
+    
+    //Convierto el número que me dan en una coordenada
+    Integer [] coordenadas = posiciones.get(posicionElegida);
+        
+  
+    //pongo la X en el tableto 
+    tablero[coordenadas[0]][coordenadas[1]] = JuegoTresEnRaya.jugador;
+    
+    pintarTablero();
+    
 }
 
 }
